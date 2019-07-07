9aeb8c64bae519d66061e6033284a15c5debf613
itsperry <perryoo11@gmail.com>
2019-07-07 14:37:18 +0200

    save

--- content/conf/conf.php
+++ content/conf/conf.php
@@ -1,10 +1,6 @@
 <?php
   //define direct path
-<<<<<<< HEAD
   define('PATH', 'https://legendary-waffle.xyz/');
-=======
-  define('PATH', 'http://localhost/mvc/');
->>>>>>> b79251fa7203853047b02ed762d170717f71e6e4
 
   //connect db
 ?>
