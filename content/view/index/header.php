adac79c84ffe42fa42119b85578b17419d73aef5
itsperry <perryoo11@gmail.com>
2019-07-07 15:19:35 +0200

    Update

--- content/view/index/header.php
+++ content/view/index/header.php
@@ -5,27 +5,45 @@
     <title>Legendary-waffle</title>
     <link rel="stylesheet" href="<?php echo constant('PATH'); ?>content/view/libs/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo constant('PATH'); ?>content/view/libs/css/mdb.min.css">
+    <script src="https://kit.fontawesome.com/163cb80f09.js"></script>
   </head>
   <body>
     <header>
-      <nav class="navbar navbar-expand-lg navbar-dark default-color">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <nav class="mb-1 navbar navbar-expand-lg navbar-dark default-color">
+        <a class="navbar-brand" href="home">Legendary-Waffle</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+          aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-              <a class="nav-link" href="home">Home</a>
+              <a class="nav-link" href="about">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="about">about</a>
+              <a class="nav-link" href="contact">Contact</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="contact">contact</a>
+              <a class="nav-link" href="portfolio">Portfolio</a>
             </li>
+          </ul>
+          <ul class="navbar-nav ml-auto nav-flex-icons">
             <li class="nav-item">
-              <a class="nav-link" href="portfolio">portfolio</a>
+              <a class="nav-link waves-effect waves-light" href="https://github.com/itsperry">
+                <i class="fab fa-github"></i>
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user"></i>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right dropdown-default"
+                aria-labelledby="navbarDropdownMenuLink-333">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
             </li>
           </ul>
         </div>
